3dc837e01a3897af80677dd7fdfc406ad7648439
Guilherme Thales <guilherme.thales@smarttbotter.com>
2020-08-03 23:02:12 -0300

    adds API integration to get header data and passes it to the SectionHero component

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,24 @@
+import { Head } from 'next/document'
+
 export type LogoProps = {
   alternativeText: string
   url: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
